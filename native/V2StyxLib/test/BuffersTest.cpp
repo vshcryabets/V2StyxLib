e2b4b7c1d867b191a4de6bdfdb0301f24c777dbe
V.Shcryabets <vshcryabets@gmail.com>
2016-10-30 22:47:45 +0200

    Update buffer test

--- native/V2StyxLib/test/BuffersTest.cpp
+++ native/V2StyxLib/test/BuffersTest.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <ctime>
 
 #include "BufferTest.h"
 #include "gtest/gtest.h"
@@ -6,7 +7,7 @@
 
 TEST(cpp_sorter_test, int_arr_sort) {
 	std::srand(std::time(0));
-	int maxSize = 8192;
+	size_t maxSize = 8192;
 	StyxByteBufferReadable *readableBuffer = new StyxByteBufferReadable(maxSize);
 	uint8_t *testBuffer = new uint8_t[maxSize];
 	uint8_t *readBuffer = new uint8_t[maxSize];
@@ -17,10 +18,10 @@ TEST(cpp_sorter_test, int_arr_sort) {
 
 	for (size_t i = 1; i < maxSize; i++) {
 		readableBuffer->write(testBuffer, 0, i);
-		int read = readableBuffer.read(readBuffer, 0, i);
-		assertEquals("Wrong read bytes count", i, read);
+		size_t read = readableBuffer->read(readBuffer, 0, i);
+		ASSERT_EQ(i, read) << "Wrong read bytes count";
 		// check read bytes
-		for (int j = 0; j < i; j++) {
+		for (size_t j = 0; j < i; j++) {
 			if (readBuffer[j] != testBuffer[j]) {
 				assertTrue("Wrong byte at position " + j, false);
 			}
