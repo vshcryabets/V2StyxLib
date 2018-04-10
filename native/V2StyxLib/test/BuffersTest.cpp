5009a6c329a706c3ccb982bcb2e3d6ef720625fe
Vladimir Shcryabets <vshcryabets@2vsoft.com>
2018-04-10 21:42:53 +0300

    Fix BufferTest

--- native/V2StyxLib/test/BuffersTest.cpp
+++ native/V2StyxLib/test/BuffersTest.cpp
@@ -7,18 +7,18 @@
 TEST(cpp_byte_buffer_readable_test, rw_test) {
 	std::srand(std::time(0));
 	size_t maxSize = 8192;
-	StyxByteBufferReadable *readableBuffer = new StyxByteBufferReadable(maxSize);
-	uint8_t *testBuffer = new uint8_t[maxSize];
-	uint8_t *readBuffer = new uint8_t[maxSize];
+	StyxByteBufferReadable readableBuffer(maxSize);
+	uint8_t testBuffer[maxSize];
+	uint8_t readBuffer[maxSize];
 
 	for (size_t i = 0; i < maxSize; i++ ) {
 		testBuffer[i] = std::rand();
 	}
 
 	for (size_t i = 1; i < maxSize; i++) {
-		readableBuffer->write(testBuffer, i);
-		ASSERT_EQ(i, readableBuffer->remainsToRead()) << "Wrong buffer size";
-		size_t read = readableBuffer->read(readBuffer, i);
+		readableBuffer.write(testBuffer, i);
+		ASSERT_EQ(i, readableBuffer.remainsToRead()) << "Wrong buffer size";
+		size_t read = readableBuffer.read(readBuffer, i);
 		ASSERT_EQ(i, read) << "Wrong read bytes count";
 		// check read bytes
 		for (size_t j = 0; j < i; j++) {
