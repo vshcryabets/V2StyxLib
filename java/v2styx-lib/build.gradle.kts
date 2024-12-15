1b1ab24add7ef9d448c9bb5c178ccc1a4281920f
Volodymyr Shkriabets <vshcryabets@gmail.com>
2024-12-15 20:46:09 +0200

    Update cgen configuration

--- java/v2styx-lib/build.gradle.kts
+++ java/v2styx-lib/build.gradle.kts
@@ -1,6 +1,28 @@
+import ce.defs.domain.DirsConfiguration
+import ce.domain.usecase.entry.BuildProjectUseCase
+import ce.domain.usecase.load.LoadMetaFilesForTargetUseCase
+import ce.domain.usecase.load.LoadProjectUseCaseImpl
+import ce.domain.usecase.load.LoadXmlTreeUseCase
+import ce.domain.usecase.store.StoreAstTreeUseCase
+import ce.domain.usecase.store.StoreOutTreeUseCase
+import ce.domain.usecase.transform.TransformInTreeToOutTreeUseCase
+import javax.script.ScriptEngineManager
+
 plugins {
     `java-library`
     id("maven-publish")
+}
+
+buildscript {
+    repositories {
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+    dependencies {
+        classpath("org.codehaus.groovy:groovy-jsr223:3.0.17")
+        classpath("com.github.vshcryabets:codegen:c88c97e217")
+    }
 }
 
 dependencies {
@@ -19,5 +41,30 @@ tasks.test {
     testLogging.showStandardStreams = true
     testLogging {
         events("passed", "skipped", "failed", "started")
+    }
+}
+
+tasks.register("runCgen") {
+    group = "Custom"
+    description = "Run code generation"
+
+    doLast {
+        val engineMaps = mapOf<ce.defs.MetaEngine, javax.script.ScriptEngine>(
+            ce.defs.MetaEngine.GROOVY to ScriptEngineManager().getEngineByName("groovy")
+        )
+        val dirsConfiguration = DirsConfiguration(
+            workingDir = rootDir.parent + "/codegen/"
+        )
+        println("CGEN Project dir = ${dirsConfiguration.workingDir}")
+        val buildProjectUseCase = BuildProjectUseCase(
+            getProjectUseCase = LoadProjectUseCaseImpl(),
+            storeInTreeUseCase = StoreAstTreeUseCase(),
+            loadMetaFilesUseCase = LoadMetaFilesForTargetUseCase(engineMaps, LoadXmlTreeUseCase()),
+            storeOutTreeUseCase = StoreOutTreeUseCase(),
+            transformInTreeToOutTreeUseCase = TransformInTreeToOutTreeUseCase(),
+        )
+        buildProjectUseCase(
+            projectFile = "project.json",
+            dirsConfiguration = dirsConfiguration)
     }
 }
