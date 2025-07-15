2cf1d0c6a27eb9ab9cc94122b66ad1146b535329
V.Shcryabets <vshkriabets@logitech.com>
2025-07-15 18:59:31 +0300

    Add StringSerializer

--- java/v2styx-lib/src/main/java/com/v2soft/styxlib/utils/StyxSessionDIImpl.java
+++ java/v2styx-lib/src/main/java/com/v2soft/styxlib/utils/StyxSessionDIImpl.java
@@ -1,23 +1,27 @@
 package com.v2soft.styxlib.utils;
 
+import com.v2soft.styxlib.l5.dev.StringSerializer;
 import com.v2soft.styxlib.l5.serialization.IDataDeserializer;
 import com.v2soft.styxlib.l5.serialization.IDataSerializer;
 import com.v2soft.styxlib.l5.serialization.impl.StyxDeserializerImpl;
 import com.v2soft.styxlib.l5.serialization.impl.StyxSerializerImpl;
+import com.v2soft.styxlib.l5.v9p2000.StringSerializerImpl;
 import com.v2soft.styxlib.l6.IsClientAuthorizedUseCase;
 import com.v2soft.styxlib.l6.IsClientAuthorizedUseCaseImpl;
 import com.v2soft.styxlib.server.ClientsRepo;
 import com.v2soft.styxlib.server.ClientsRepoImpl;
 
 public class StyxSessionDIImpl implements StyxSessionDI {
-    private ClientsRepo mClientsRepo;
-    private IDataSerializer serializer = new StyxSerializerImpl();
-    private IDataDeserializer deserializer = new StyxDeserializerImpl();
-    private IsClientAuthorizedUseCaseImpl isClientAuthorizedUseCase;
+    private final ClientsRepo mClientsRepo;
+    private final IDataSerializer serializer = new StyxSerializerImpl();
+    private final IDataDeserializer deserializer = new StyxDeserializerImpl();
+    private final IsClientAuthorizedUseCaseImpl isClientAuthorizedUseCase;
+    private final StringSerializer stringSerializer;
 
     public StyxSessionDIImpl(boolean authRequired) {
         mClientsRepo = new ClientsRepoImpl(new ClientsRepoImpl.Configuration(!authRequired));
         isClientAuthorizedUseCase = new IsClientAuthorizedUseCaseImpl(mClientsRepo);
+        stringSerializer = new StringSerializerImpl();
     }
 
     @Override
@@ -38,5 +42,10 @@ public class StyxSessionDIImpl implements StyxSessionDI {
     @Override
     public ClientsRepo getClientsRepo() {
         return mClientsRepo;
+    }
+
+    @Override
+    public StringSerializer getStringSerializer() {
+        return stringSerializer;
     }
 }
