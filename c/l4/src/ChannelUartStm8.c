9012896d6121d1765c1a91ff20002402f26e78ce
V.Shkriabets <vshcryabets@gmail.com>
2026-03-19 23:24:58 +0200

    Refactor soft UART implementation for STM8: update pin handling and add delay function

--- c/l4/src/ChannelUartStm8.c
+++ c/l4/src/ChannelUartStm8.c
@@ -1,12 +1,43 @@
 #include "ChannelUartStm8.h"
 
+#ifdef V2STYXLIB_SOFTUART
+void v2styxlib_delay_bit() {
+    __asm
+        pushw x         
+        ldw x, #535 ; hardcode for 9600 baud at 16 MHz, adjust if needed
+    00001$:
+        decw x  
+        jrne 00001$  
+        popw x  
+    __endasm;
+}
+#endif
+
 void v2styxlib_send_byte(
     const V2styxlibUartStm8Config* config,
     uint8_t byte) {
 #ifdef V2STYXLIB_SOFTUART
     if (config->baseConfig.config & V2STYXLIB_CONFIG_SOFT_UART_TX) {
         // Send byte using software UART
-    }
+        // Start bit
+        config->softUartPort->ODR &= ~config->softUartTxPinMask;
+        v2styxlib_delay_bit();
+
+        // Data bits (LSB first)
+        for (uint8_t i = 0; i < 8; i++) {
+            if (byte & 0x01) {
+                config->softUartPort->ODR |= config->softUartTxPinMask;
+            } else {
+                config->softUartPort->ODR &= ~config->softUartTxPinMask;
+            }
+            byte >>= 1;
+            v2styxlib_delay_bit();
+        }
+
+        // Stop bit
+        config->softUartPort->ODR |= config->softUartTxPinMask;
+        v2styxlib_delay_bit();
+    } else
 #endif
     {    
         while (!(UART1->SR & UART1_SR_TXE)) {
@@ -26,9 +57,10 @@ void v2styxlib_uart_setup(
 #ifdef V2STYXLIB_SOFTUART
      if (config->baseConfig.config & V2STYXLIB_CONFIG_SOFT_UART_TX) {
         // Configure the soft UART TX pin as output
-        config->softUartPort->DDR |= (1 << config->softUartTxPin);
-        // Set the soft UART TX pin high (idle state)
-        config->softUartPort->ODR |= (1 << config->softUartTxPin);
+        config->softUartPort->DDR |= config->softUartTxPinMask;
+        config->softUartPort->CR1 |= config->softUartTxPinMask;
+        config->softUartPort->CR2 |= config->softUartTxPinMask;
+        config->softUartPort->ODR |= config->softUartTxPinMask;
         UART1->CR2 &= ~UART1_CR2_TEN; // Disable hardware UART transmission, we will use software UART for TX
         UART1->CR2 |= UART1_CR2_REN;
     } else 
