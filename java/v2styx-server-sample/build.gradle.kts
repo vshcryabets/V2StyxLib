a1476d006d8c5465ad9775a182144a91eaa1ab97
Volodymyr Shkriabets <vshcryabets@gmail.com>
2026-08-22 11:36:47 +0300

    Update gradle (#61)
    * Update gradle
    
    * Versions catalog

--- java/v2styx-server-sample/build.gradle.kts
+++ java/v2styx-server-sample/build.gradle.kts
@@ -6,25 +6,30 @@ import ce.domain.usecase.store.StoreAstTreeUseCase
 import ce.domain.usecase.store.StoreOutTreeUseCase
 import ce.domain.usecase.transform.TransformInTreeToOutTreeUseCase
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import javax.script.ScriptEngineManager
 
-val jdkLevel: JavaLanguageVersion by rootProject.extra
+val libsCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
+val jdkLevel = rootProject.extra["jdkLevel"] as JavaLanguageVersion
+val shadowArchiveVersion = libsCatalog.findVersion("shadow-archive").get().requiredVersion
 
 plugins {
     java
     application
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    alias(libs.plugins.shadow)
 }
 
 buildscript {
+    val libs = project.extensions.getByType<VersionCatalogsExtension>().named("libs")
+
     repositories {
         maven {
             url = uri("https://jitpack.io")
         }
     }
     dependencies {
-        classpath("org.codehaus.groovy:groovy-jsr223:3.0.17")
-        classpath("com.github.vshcryabets:codegen:4895044cf9")
+        classpath(libs.findLibrary("groovy-jsr223").get())
+        classpath(libs.findLibrary("codegen-server-sample").get())
     }
 }
 
@@ -35,17 +40,15 @@ java {
     }
 }
 
-val version = "1.0.0"
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
     implementation(project(":v2styx-lib"))
-    implementation("org.jline:jline:3.25.0")
-    implementation("com.fasterxml.jackson.core:jackson-core:2.13.4")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
+    implementation(libs.jline)
+    implementation(libs.jackson.core)
+    implementation(libs.jackson.databind)
 }
 
 application {
@@ -55,7 +58,7 @@ application {
 tasks {
     named<ShadowJar>("shadowJar") {
         archiveBaseName.set("folder-server")
-        archiveVersion.set("0.1.0")
+        archiveVersion.set(shadowArchiveVersion)
         mergeServiceFiles()
         manifest {
             attributes(mapOf("Main-Class" to "com.v2soft.folderserver.FolderServerSample"))
